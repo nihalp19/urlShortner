9b2af512e1343aa2e573029ba571f7cb609b08f8
Nihal Panday <nihalpanday2020@gmail.com>
2025-04-10 23:30:57 +0530

    updated backend

--- backend/lib/getCountryFromIP.js
+++ backend/lib/getCountryFromIP.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-
-
-export const getCountryFromIP = async () => {
+export const getCountryFromIP = async (req) => {
   try {
-    const { data } = await axios.get('https://ipapi.co/json/');
+    const forwarded = req.headers['x-forwarded-for'];
+    const ip = forwarded ? forwarded.split(',')[0] : req.socket.remoteAddress;
+
+    const { data } = await axios.get(`https://ipapi.co/${ip}/json/`);
     return data?.country_name || 'Unknown';
   } catch (error) {
     console.error('Error fetching IP geolocation:', error.message);
