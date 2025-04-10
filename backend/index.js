eb04bfd79ffede156947da229961295abc173cff
Nihal Panday <nihalpanday2020@gmail.com>
2025-04-10 22:24:56 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,8 @@ const app = express()
 
 app.use(cors({
     origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
 
