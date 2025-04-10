9df37450f8ac38510b7827a70353456bda3a6898
Nihal Panday <nihalpanday2020@gmail.com>
2025-04-10 23:05:25 +0530

    some changes

--- frontend/src/store/userAuthStore.js
+++ frontend/src/store/userAuthStore.js
@@ -2,55 +2,54 @@ import { create } from "zustand";
 import { axiosInstance } from "../axios/axiosInstance";
 import toast from "react-hot-toast";
 
-export const userAuthStore = create((set,get) => ({
-    user : null,
-    loading : true,
-    isSideBarOpen : false,
+export const userAuthStore = create((set, get) => ({
+    user: null,
+    loading: true,
+    isSideBarOpen: false,
 
 
     setisSideBarOpen: () => {
         set((state) => ({ isSideBarOpen: !state.isSideBarOpen }));
     },
 
-    login : async({email,password}) => {
-        set({loading : true})
+    login: async ({ email, password }) => {
+        set({ loading: true })
         try {
-            const res = await axiosInstance.post('/login',{email,password})
-            set({user : res.data.user})
+            const res = await axiosInstance.post('/login', { email, password })
+            set({ user: res.data.user })
             console.log(res)
             toast.success("Login SuccessFull")
         } catch (error) {
-            console.log("error while login",error.message)
-        }finally{
-            set({loading : false})
+            console.log("error while login", error.message)
+        } finally {
+            set({ loading: false })
         }
     },
 
-    logout : async() => {
+    logout: async () => {
         console.log("hi")
-        set({loading : true})
+        set({ loading: true })
         try {
             const res = await axiosInstance.post('/logout')
-            set({user : null})
+            set({ user: null })
             toast.success("Logout Successfull")
         } catch (error) {
-            console.log("error while logout",error.message)
-        }finally{
-            set({loading : false})
+            console.log("error while logout", error.message)
+        } finally {
+            set({ loading: false })
         }
     },
 
-    checkAuth : async() => {
-        set({loading : true})
+    checkAuth: async () => {
+        set({ loading: true })
         try {
             const res = await axiosInstance.get("/checkauth")
-            set({user : res.data.user})
+            set({ user: res.data.user })
         } catch (error) {
-            toast.error("user is not unauthorized")
-            console.log("error while checkauth",error.message)
+            console.log("error while checkauth", error.message)
         }
-        finally{
-            set({loading : false})
+        finally {
+            set({ loading: false })
         }
     }
 }))
